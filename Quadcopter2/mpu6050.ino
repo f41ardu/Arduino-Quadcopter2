1e5401165f53c465ceac54dfe2baee13419eacea
f41ardu <f41thr@arcor.de>
2017-02-04 21:19:10 +0100

    Finaly removed mpu_update
    Finaly removed mpu_update in mpu6050.ino

--- Quadcopter2/mpu6050.ino
+++ Quadcopter2/mpu6050.ino
@@ -98,43 +98,6 @@ void mpu_init() {
   }
 }
 // ================================================================
-// ===               MPU Update ROUTINE                         ===
-// ================================================================
-/*
-void mpu_update() {
-  // Quaternions and parameters for 6 DoF sensor fusion calculations
-  Quaternion q;          // [w, x, y, z]         quaternion container
-  // if programming failed, don't try to do anything
-  if (!dmpReady) return;
-  // wait for MPU interrupt or extra packet(s) available
-  while (!mpuInterrupt && fifoCount < packetSize) {
-    // do nothing here
-  }
-  // reset interrupt flag and get INT_STATUS byte
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
-  // get current FIFO count
-  fifoCount = mpu.getFIFOCount();
-  // check for overflow (this should never happen unless our code is too inefficient)
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-    // reset so we can continue cleanly
-    mpu.resetFIFO();
-    //    Serial.println(F("FIFO overflow!"));
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-  } else if (mpuIntStatus & 0x02) {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-    // read a packet from FIFO
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-    // track FIFO count here in case there is > 1 packet available
-    // (this lets us immediately read more without waiting for an interrupt)
-    fifoCount -= packetSize;
-    // get quaternion values in InvenSense Teapot format and store them in q
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-  }
-}
-*/ 
-// ================================================================
 // ===               MPU Calculate YPR ROUTINE                  ===
 // ================================================================
 void  mpu_ypr() {
